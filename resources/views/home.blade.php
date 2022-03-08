6bfe3b512573971839f413803718d66d249d2599
Walter Gutierrez <elterrible8596@hotmail.com>
2022-03-08 13:05:43 -0400

    project re-vamp

--- resources/views/home.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-
-<h1 class="fw-bold">Bienvenido! {{Auth::user()->usuario}}</h1>
-    <p>Este es un modal de administrador</p>
-    <script src="{{ asset('js/app.js') }}"></script>
-
-@endsection
