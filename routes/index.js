608b8402eeecf86b63f44e741c5f05b7de455361
Walter Gutierrez <elterrible8596@hotmail.com>
2022-03-17 15:58:34 -0400

    Updating UI

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Login  | SGDE' });
 });
 
+router.get('/register', function(req, res, next) {
+  res.render('auth/register', { title: 'Registro | SGDE' });
+});
 
 module.exports = router;
